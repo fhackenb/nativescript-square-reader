4e1d1acd1421eb6d212b31cd74dc7fc30a92a30c
fhackenb <fchack93@gmail.com>
2018-10-18 16:22:04 -0400

    Clean up console logs

--- demo/app/main-view-model.ts
+++ demo/app/main-view-model.ts
@@ -38,11 +38,8 @@ export class HelloWorldModel {
       this.authorizeLocation()
       .then( res => {
         this.squareReader = new SquareReader();
-        console.log("Square reader {N}:", this.squareReader);
-        console.log("Prepare to authorize with code:", this.code);
         this.squareReader.authenticate(this.code)
           .then( (res: SquareAuthStatus) => {
-            console.log("Status:", res);
             if (res.code === 0) {
               this.isSquareAuthenticated = true;
               alert("Square Authenticated!");
@@ -52,29 +49,20 @@ export class HelloWorldModel {
             }
           })
           .catch( (err) => {
-            console.log("Auth error:", err);
             alert("Auth error status:" + err.code);
           });
         })
         .catch( err => {
-          console.log("Erorr in location access:", err);
           alert("Location error:" + err.code);
         })
   }
 
   public checkout() {
-    console.log("Attempt checkout");
     if (!this.isSquareAuthenticated) {
       return alert("Not authenticated!");
     }
-    let uiVC = this.page.ios;
-    console.log("This page controller:", uiVC);
-    let subscription = this.squareReader.startCheckout(100, uiVC);
-    console.log("Subscription:", subscription);
+    let subscription = this.squareReader.startCheckout(100, this.page.ios);
     subscription.subscribe( (result: SquareCheckoutResult) => {
-      console.log("Subscription:", result);
-      console.log("message:", result.message);
-      console.log("Status:", result.status);
       alert(result.message);
       switch (result.status) {
         case 1:
@@ -86,7 +74,7 @@ export class HelloWorldModel {
         case 0:
           console.log("Succeeded!");
           console.log("LocationId:", result.checkoutResult.locationID);
-          console.log("TransactionId:", result.checkoutResult.tenders);
+          console.log("Tenders:", result.checkoutResult.tenders);
           break;
       }
     });
